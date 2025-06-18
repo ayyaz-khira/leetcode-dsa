2a446e744b3692af2fc93bee295e6297e5a7f58e
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-06-18 14:51:23 +0530

    Time: 4 ms (100%), Space: 44.2 MB (37.64%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0) return false;
+        int num=x;
+        int rev=0;
+        while(x!=0){
+            int rem=x%10;
+            rev=rev*10+rem;
+            x=x/10;
+        }
+
+        if(num==rev){
+            return true;
+        }
+
+        return false;
+
+        
+        
+    }
+}
