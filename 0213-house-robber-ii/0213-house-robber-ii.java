95964b3104e446306de4598313139fd292e4fbc3
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-07-06 15:52:59 +0530

    Time: 0 ms (100%), Space: 41.5 MB (10.47%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,32 +1,47 @@
 class Solution {
 
-    public int robHelper(int arr[]) {
-        
-        int n = arr.length;
-        if (n == 0) return 0;
-        if (n == 1) return arr[0];
+    public int robber(int arr[]){
+
+        int n=arr.length;
         int dp[]=new int[n];
+
+        if(arr.length==1){
+            return arr[0];
+        }
+        if(arr.length==2){
+            return Math.max(arr[0],arr[1]);
+        }
+
         dp[0]=arr[0];
         dp[1]=Math.max(arr[0],arr[1]);
+
+    
+
         for(int i=2;i<n;i++){
-            dp[i]=Math.max(dp[i-2]+arr[i],dp[i-1]);
+            dp[i]=Math.max(dp[i-1],arr[i]+dp[i-2]);
         }
-        
+
         return dp[n-1];
+
     }
 
+    public int rob(int[] arr) {
 
-    public int rob(int[] nums) {
+        int n=arr.length;
+        int dp[]=new int[n];
 
-        int n=nums.length;
-        if(n==0) return 0;
-        if(n==1) return nums[0];
+        if(arr.length==1){
+            return arr[0];
+        }
 
+        if(arr.length==2){
+            return Math.max(arr[0],arr[1]);
+        }
 
-        int maxExceptLast=robHelper(Arrays.copyOfRange(nums,0,n-1));
-        int maxExceptFirst=robHelper(Arrays.copyOfRange(nums,1,n));
+        int firstPart=robber(Arrays.copyOfRange(arr,0,arr.length-1));
+        int secondPart=robber(Arrays.copyOfRange(arr,1,arr.length));
 
-        return Math.max(maxExceptLast,maxExceptFirst);
+        return Math.max(firstPart,secondPart);
         
     }
 }
