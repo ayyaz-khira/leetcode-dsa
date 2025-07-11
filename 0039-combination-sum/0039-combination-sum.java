9f195e63a7064888331ad8838edb0115cad8c483
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-07-11 13:18:38 +0530

    Time: 2 ms (83.03%), Space: 44.7 MB (46.08%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,28 +1,28 @@
 class Solution {
-
     public void Permute(int arr[],int index,int target,List<List<Integer>> res,List<Integer> comb){
         if(target==0){
-            res.add(new ArrayList(comb));
+            List<Integer> temp=new ArrayList<>();
+            for(int x:comb){
+                temp.add(x);
+            }
+            res.add(temp);
             return;
         }
-
-        if(target<0 || index==arr.length){
+        if(target<0 || index>=arr.length){
             return;
         }
 
         comb.add(arr[index]);
         Permute(arr,index,target-arr[index],res,comb);
         comb.remove(comb.size()-1);
-        Permute(arr,index+1,target,res,comb);
 
+        Permute(arr,index+1,target,res,comb);
     }
 
-
-    public List<List<Integer>> combinationSum(int[] arr, int target) {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> res=new ArrayList<>();
         List<Integer> comb=new ArrayList<>();
-        Permute(arr,0,target,res,comb);
-
+        Permute(candidates,0,target,res,comb);
         return res;
     }
 }
