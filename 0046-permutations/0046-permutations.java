8300b9d74cc72b844814d6894b00823abd840aae
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-07-11 13:09:08 +0530

    Time: 1 ms (93.13%), Space: 45 MB (10.6%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,35 +1,29 @@
 class Solution {
-
     public void swap(int arr[],int a,int b){
         int temp=arr[a];
         arr[a]=arr[b];
         arr[b]=temp;
     }
-
+    
     public void Permute(int arr[],int start,int end,List<List<Integer>> res){
-        if(start==end){
-            ArrayList<Integer> comb=new ArrayList<>();
+        if(start>=end){
+            ArrayList<Integer> temp=new ArrayList<>();
             for(int x:arr){
-                comb.add(x);
+                temp.add(x);
             }
-            res.add(comb);
+            res.add(temp);
             return;
         }
-
+        
         for(int i=start;i<=end;i++){
             swap(arr,start,i);
             Permute(arr,start+1,end,res);
             swap(arr,start,i);
         }
     }
-
-
-
-
-    public List<List<Integer>> permute(int[] arr) {
+    public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res=new ArrayList<>();
-        Permute(arr,0,arr.length-1,res);
-
+        Permute(nums,0,nums.length-1,res);
         return res;
     }
 }
