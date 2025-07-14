9e50cea09690771993a1c075202b0119c887f9cd
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-07-14 18:48:22 +0530

    Time: 2 ms (82.93%), Space: 44.5 MB (92.28%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,28 +1,31 @@
 class Solution {
-    public void Permute(int arr[],int index,int target,List<List<Integer>> res,List<Integer> comb){
+
+    public void Permute(int arr[],int ind,int target,List<List<Integer>> res,List<Integer> comb){
         if(target==0){
-            List<Integer> temp=new ArrayList<>();
-            for(int x:comb){
-                temp.add(x);
-            }
-            res.add(temp);
-            return;
-        }
-        if(target<0 || index>=arr.length){
+            res.add(new ArrayList(comb));
             return;
         }
 
-        comb.add(arr[index]);
-        Permute(arr,index,target-arr[index],res,comb);
+        if(target<0 || ind==arr.length){
+            return;
+        }
+
+        comb.add(arr[ind]);
+        Permute(arr,ind,target-arr[ind],res,comb);
         comb.remove(comb.size()-1);
 
-        Permute(arr,index+1,target,res,comb);
+
+        Permute(arr,ind+1,target,res,comb);
     }
+
+
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> res=new ArrayList<>();
         List<Integer> comb=new ArrayList<>();
+
         Permute(candidates,0,target,res,comb);
+
         return res;
     }
 }
