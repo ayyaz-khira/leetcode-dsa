266473f34b6afd50988ff8b223ce19e5e7ec33a8
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-07-15 19:15:53 +0530

    Time: 8 ms (45.14%), Space: 41.2 MB (6.69%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,20 +1,12 @@
 class Solution {
 
     public int fibo(int n){
-        if(n==0 || n==1){
-            return n;
-        }
+        if(n==0 || n==1) return n;
 
         return fibo(n-1)+fibo(n-2);
     }
 
-
-
-
-
     public int fib(int n) {
-        int fibonacciNum=fibo(n);
-        return fibonacciNum;
-        
+        return fibo(n);
     }
 }
