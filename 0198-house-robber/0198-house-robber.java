f9facc20dd05cdddbb276ea3ecf328a2b42ea289
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-07-15 21:44:32 +0530

    Time: 0 ms (100%), Space: 41.2 MB (35.1%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,9 +1,10 @@
 class Solution {
     public int rob(int[] nums) {
         int n=nums.length;
+
         if(n==0) return 0;
-        if(nums.length==1) return nums[0];
-        if(nums.length==2) return Math.max(nums[0],nums[1]);
+        if(n==1) return nums[0];
+        if(n==2) return Math.max(nums[0],nums[1]);
 
         int dp[]=new int[n];
         dp[0]=nums[0];
@@ -12,7 +13,6 @@ class Solution {
         for(int i=2;i<n;i++){
             dp[i]=Math.max(dp[i-1],nums[i]+dp[i-2]);
         }
-
         return dp[n-1];
     }
 }
