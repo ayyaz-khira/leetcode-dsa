09ffcefa61a63b104fa8d69a87c636360617433c
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-07-25 15:51:16 +0530

    Time: 11 ms (33.71%), Space: 58.9 MB (93.63%) - LeetHub

--- 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.java
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.java
@@ -1,35 +1,31 @@
 class Solution {
     public int minSwaps(int[] arr) {
         int n=arr.length;
-        
         int totalOnes=0;
         int curOnes=0;
-        int l=0;
-        int max=0;
-        
-        
+
         for(int i=0;i<n;i++){
             if(arr[i]==1){
                 totalOnes++;
             }
         }
 
-        
-        if(totalOnes==0 || totalOnes==n) return 0;
-        
+        int l=0;
+        int maxOnes=0;
+
         for(int r=0;r<2*n;r++){
             if(arr[r%n]==1){
                 curOnes++;
             }
-            
+
             if(r-l+1>totalOnes){
                 curOnes-=arr[l%n];
                 l++;
             }
-            
-            max=Math.max(max,curOnes);
+
+            maxOnes=Math.max(curOnes,maxOnes);
         }
-        return totalOnes-max;
-        
+
+        return totalOnes-maxOnes;
     }
 }
