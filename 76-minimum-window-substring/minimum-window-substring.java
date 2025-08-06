7f55a70bd758a884f6d21f7fea8bb39c211c0c4c
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-08-06 14:38:52 +0530

    Time: 100 ms (17.64%) | Memory: 45.8 MB (11.35%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,22 +1,23 @@
 class Solution {
     public String minWindow(String s, String t) {
         int n=s.length();
-
+        int minLen=Integer.MAX_VALUE;
         int freq[]=new int[128];
+        int count=0;
+        int l=0;
+        String ans="";
+
         for(char c:t.toCharArray()){
             freq[c]++;
         }
-        int count=0;
-        int minLen=Integer.MAX_VALUE;
-        int l=0;
-        String ans="";
+
+
         for(int r=0;r<n;r++){
             char c=s.charAt(r);
             freq[c]--;
             if(freq[c]>=0){
                 count++;
             }
-
 
             while(count==t.length()){
                 char leftChar=s.charAt(l);
@@ -33,8 +34,7 @@ class Solution {
             }
         }
 
-
         return ans;
-        
+
     }
 }
