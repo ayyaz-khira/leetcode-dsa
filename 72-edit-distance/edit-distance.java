0bb70207c3b4cec933196cedb9711acf3ac77a46
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-08-10 15:02:46 +0530

    Time: 5 ms (60.48%) | Memory: 45 MB (62.82%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,7 +1,9 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int n=word1.length();
         int m=word2.length();
+        int n=word1.length();
+
+
         int dp[][]=new int[m+1][n+1];
 
         for(int i=0;i<=m;i++){
@@ -14,11 +16,11 @@ class Solution {
 
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
-                if(word1.charAt(j-1)==word2.charAt(i-1)){
+                if(word2.charAt(i-1)==word1.charAt(j-1)){
                     dp[i][j]=dp[i-1][j-1];
                 }
                 else{
-                    dp[i][j]= 1 + Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]));
+                    dp[i][j]= 1+ Math.min(dp[i-1][j-1],Math.min(dp[i][j-1],dp[i-1][j]));
                 }
             }
         }
