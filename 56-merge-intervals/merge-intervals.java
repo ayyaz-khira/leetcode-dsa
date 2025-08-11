a3c70c5346da096a4f02a7358f898990f3ce8ab0
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-08-11 07:56:02 +0530

    Time: 10 ms (20.97%) | Memory: 47.2 MB (6.17%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,35 +1,31 @@
-class IntervalComparator implements Comparator<int[]>{
-    @Override
-    public int compare(int arr1[],int arr2[]){
-        return arr1[0]-arr2[0];
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals,new SortIntervals());
+        List<int[]> res=new ArrayList<>();
+        for(int[] interval:intervals){
+            if(res.isEmpty() || res.get(res.size()-1)[1]<interval[0]){
+                res.add(interval);
+            }
+            else{
+                res.get(res.size()-1)[1]=Math.max(res.get(res.size()-1)[1],interval[1]);
+            }
+        }
+
+        int list[][]=new int[res.size()][];
+        int i=0;
+        for(int[] interval:res){
+            list[i++]=interval;
+        }
+
+        return list;
+        
     }
 }
 
 
-class Solution {
-
-    public int[][] merge(int[][] intervals) {
-
-        Arrays.sort(intervals,new IntervalComparator());
-        List<int[]> list=new ArrayList<>();
-
-        for(int[] interval:intervals){
-            if(list.isEmpty() || list.get(list.size()-1)[1]<interval[0]){
-                list.add(interval);
-            }
-            else{
-                list.get(list.size()-1)[1]=Math.max(list.get(list.size()-1)[1],interval[1]);
-            }
-        }
-
-        int arr[][]=new int[list.size()][];
-        int j=0;
-        for(int[] x:list){
-            arr[j]=x;
-            j++;
-        }
-
-        return arr;
-        
+class SortIntervals implements Comparator<int[]>{
+    @Override
+    public int compare(int arr1[],int arr2[]){
+        return arr1[0]-arr2[0];
     }
 }
