73d1aeb5d1bc400b61ab36a869a10aad25542723
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-08-20 16:07:31 +0530

    Time: 5 ms (60.66%), Space: 45.4 MB (10.68%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -4,14 +4,14 @@ class Solution {
         int n=word2.length();
 
         int dp[][]=new int[m+1][n+1];
-
+        
         for(int i=0;i<=m;i++){
             dp[i][0]=i;
         }
-        for(int j=0;j<=n;j++){
+
+        for(int j=1;j<=n;j++){
             dp[0][j]=j;
         }
-
 
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
@@ -24,8 +24,7 @@ class Solution {
             }
         }
 
+
         return dp[m][n];
-
-
     }
 }
