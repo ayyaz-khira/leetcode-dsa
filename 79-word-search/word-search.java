d44d1d1f2319b2f80a724f241f3887738de73eac
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-08-21 21:43:43 +0530

    Time: 163 ms (32.87%) | Memory: 41.6 MB (62.06%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,26 +1,25 @@
 class Solution {
 
-    public boolean solve(char mat[][],int row,int col,int rows,int cols,String word,int ind){
+    public boolean find(char mat[][],int row,int col,int rows,int cols,int ind,String word){
         if(ind==word.length()) return true;
 
-        if(row<0 || row>=rows || col<0 || col>=cols || mat[row][col]=='#' || mat[row][col]!=word.charAt(ind)){
-            return false;
-        }
+        if(row<0 || row>=rows || col<0 || col>=cols || word.charAt(ind)=='#' || word.charAt(ind)!=mat[row][col]) return false;
 
         char temp=mat[row][col];
         mat[row][col]='#';
 
-        boolean found= solve(mat,row+1,col,rows,cols,word,ind+1) ||
-                       solve(mat,row-1,col,rows,cols,word,ind+1) ||
-                       solve(mat,row,col+1,rows,cols,word,ind+1) ||
-                       solve(mat,row,col-1,rows,cols,word,ind+1);
+        boolean found=find(mat,row+1,col,rows,cols,ind+1,word) ||
+                      find(mat,row-1,col,rows,cols,ind+1,word) ||
+                      find(mat,row,col+1,rows,cols,ind+1,word) ||
+                      find(mat,row,col-1,rows,cols,ind+1,word);
 
         mat[row][col]=temp;
 
         return found;
 
-
+            
     }
+
 
 
     public boolean exist(char[][] board, String word) {
@@ -29,11 +28,12 @@ class Solution {
 
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
-                if(board[i][j]==word.charAt(0)){
-                    if(solve(board,i,j,rows,cols,word,0)) return true;
+                if(word.charAt(0)==board[i][j]){
+                    if(find(board,i,j,rows,cols,0,word)) return true;
                 }
             }
         }
+
 
 
         return false;
