54f65d215f4c173ab5ba5e6274edfd0f9efd6061
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-08-24 16:08:42 +0530

    Time: 5 ms (60.57%), Space: 45 MB (63.31%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -4,8 +4,9 @@ class Solution {
         int n=word2.length();
 
         int dp[][]=new int[m+1][n+1];
-        
-        for(int i=0;i<=m;i++){
+        dp[0][0]=0;
+
+        for(int i=1;i<=m;i++){
             dp[i][0]=i;
         }
 
@@ -13,18 +14,21 @@ class Solution {
             dp[0][j]=j;
         }
 
+
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
                 if(word1.charAt(i-1)==word2.charAt(j-1)){
                     dp[i][j]=dp[i-1][j-1];
                 }
                 else{
-                    dp[i][j]=1+Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]));
+                    dp[i][j]=1+ Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]));
                 }
             }
         }
 
 
         return dp[m][n];
+
+
     }
 }
