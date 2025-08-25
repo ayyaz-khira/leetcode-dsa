2fea91b8d5814d4ba6e5f81ba7fddc238b8c885e
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-08-25 19:28:18 +0530

    Time: 1 ms (99.75%) | Memory: 44.5 MB (84.56%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.java
+++ 113-path-sum-ii/path-sum-ii.java
@@ -15,35 +15,28 @@
  */
 class Solution {
 
-    public void solve(TreeNode root,int target,List<Integer> arr,List<List<Integer>> res){
-        if(root==null){
-            return;
-        }
+    public void solve(TreeNode root,int target,List<Integer> comb,List<List<Integer>> res){
+        if(root==null) return;
 
-        arr.add(root.val);
+        comb.add(root.val);
         target-=root.val;
 
         if(target==0 && root.left==null && root.right==null){
-            res.add(new ArrayList<>(arr));
-            
+            res.add(new ArrayList<>(comb));
         }
 
-        solve(root.left,target,arr,res);
-        solve(root.right,target,arr,res);
-        arr.remove(arr.size()-1);
+        solve(root.left,target,comb,res);
+        solve(root.right,target,comb,res);
 
 
-
+        comb.remove(comb.size()-1);
     }
 
 
-
-    public List<List<Integer>> pathSum(TreeNode root, int target) {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<Integer> comb=new ArrayList<>();
         List<List<Integer>> res=new ArrayList<>();
-        List<Integer> arr=new ArrayList<>();
-        solve(root,target,arr,res);
-
-
+        solve(root,targetSum,comb,res);
         return res;
     }
 }
