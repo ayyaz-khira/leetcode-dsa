a37c0291b16c6670e4adfd4ca64fd422d6e269f4
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-08-30 11:06:37 +0530

    Time: 18 ms (79.09%) | Memory: 51.5 MB (30.48%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -8,10 +8,10 @@ class Solution {
         for(int i=0;i<=m;i++){
             dp[i][0]=1;
         }
-
         for(int j=1;j<=n;j++){
             dp[0][j]=0;
         }
+
 
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
@@ -20,10 +20,11 @@ class Solution {
                 }
                 else{
                     dp[i][j]=dp[i-1][j];
-
                 }
             }
         }
+
+
 
         return dp[m][n];
     }
