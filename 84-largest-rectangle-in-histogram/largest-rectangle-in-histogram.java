92809fdbff9e50c10a60b3f03da860b52ef99328
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-09-01 19:47:48 +0530

    Time: 60 ms (83.34%) | Memory: 57.1 MB (88.43%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -7,17 +7,14 @@ class Solution {
         for(int i=0;i<=n;i++){
             int curHeight=(i==n)? 0 : heights[i];
 
-            while(!stk.isEmpty() && curHeight<heights[stk.peek()]){
+            while(!stk.isEmpty() && curHeight<=heights[stk.peek()]){
                 int height=heights[stk.pop()];
-                int width=(stk.isEmpty())? i : i-stk.peek()-1;
-
+                int width=stk.isEmpty()? i : i-stk.peek()-1;
                 max=Math.max(max,height*width);
 
             }
-
             stk.push(i);
         }
-
 
         return max;
     }
