23530ade42e9872e775d0388348a9b06721b38d3
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-09-04 19:56:12 +0530

    Time: 8 ms (85.34%), Space: 47 MB (16.13%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,30 +1,37 @@
-class Solution {
-    public int[][] merge(int[][] intervals) {
-        List<int[]> res=new ArrayList<>();
-        Arrays.sort(intervals,new IntervalSort());
-        
-
-        for(int[] interval:intervals){
-            if(res.isEmpty() || res.get(res.size()-1)[1]<interval[0]){
-                res.add(interval);
-            }
-            else{
-                res.get(res.size()-1)[1]=Math.max(res.get(res.size()-1)[1],interval[1]);
-            }
-        }
-
-        int arr[][]=new int[res.size()][];
-        for(int i=0;i<res.size();i++){
-            arr[i]=res.get(i);
-        }
-
-        return arr;
-        
+class CustomComparator implements Comparator<int[]>{
+    public int compare(int a[],int b[]){
+        return Integer.compare(a[0],b[0]);
     }
 }
 
-class IntervalSort implements Comparator<int[]>{
-    public int compare(int arr1[],int arr2[]){
-        return arr1[0]-arr2[0];
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals,new CustomComparator());
+        int n=intervals.length;
+        List<int[]> list=new ArrayList<>();
+        for(int x[]:intervals){
+            if(list.isEmpty() || x[0]>list.get(list.size()-1)[1]){
+                list.add(x);
+            }
+            else{
+                list.get(list.size()-1)[1]=Math.max(list.get(list.size()-1)[1],x[1]);
+            }
+        }
+
+
+        int result[][]=new int[list.size()][];
+        int j=0;
+        for(int x[]:list){
+            result[j++]=x;
+
+
+        }
+
+
+        return result;
+
+
+
+
     }
 }
