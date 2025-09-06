6673e61684bb5840359fb24256c90837a529c787
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-09-06 22:15:06 +0530

    Time: 2 ms (48.69%), Space: 47.6 MB (67.26%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -0,0 +1,46 @@
+class Solution {
+
+    public void dfs(int node,boolean vis[],ArrayList<ArrayList<Integer>> adj){
+        vis[node]=true;
+
+        for(int x:adj.get(node)){
+            if(!vis[x]){
+                dfs(x,vis,adj);
+            }
+        }
+    }
+
+
+
+    public int findCircleNum(int[][] isConnected) {
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<isConnected.length;i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int i=0;i<isConnected.length;i++){
+            for(int j=0;j<isConnected.length;j++){
+                if(i!=j && isConnected[i][j]==1){
+                    adj.get(i).add(j);
+                }
+            }
+        }
+
+        boolean vis[]=new boolean[adj.size()];
+        int provinces=0;
+        for(int i=0;i<adj.size();i++){
+            if(!vis[i]){
+                dfs(i,vis,adj);
+                provinces++;
+
+            }
+        }
+
+
+        return provinces;
+
+        
+
+
+    }
+}
