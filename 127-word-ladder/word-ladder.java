7492fcf3d35570f5b05850fa25acca2261aa7683
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-09-07 12:18:54 +0530

    Time: 72 ms (71.34%) | Memory: 46 MB (84.41%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -2,46 +2,42 @@ class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         HashSet<String> set=new HashSet<>();
         Queue<String> queue=new LinkedList<>();
-
-        if(!wordList.contains(endWord)){
-            return 0;
-        }
-
         for(String x:wordList){
             set.add(x);
         }
 
-        int depth=1;
+        if(!wordList.contains(endWord)) return 0;
+
         queue.offer(beginWord);
+        int depth=1;
 
         while(!queue.isEmpty()){
             int size=queue.size();
             for(int k=0;k<size;k++){
                 String word=queue.poll();
                 char arr[]=word.toCharArray();
-                for(int i=0;i<word.length();i++){
+                for(int i=0;i<arr.length;i++){
                     char temp=arr[i];
                     for(char j='a';j<='z';j++){
+                        if(temp==j) continue;
                         arr[i]=j;
                         String newWord=String.valueOf(arr);
-                        if(j==temp) continue;
-                        if(set.contains(newWord)){
-                            queue.offer(newWord);
-                            set.remove(newWord);
-                        }
                         if(newWord.equals(endWord)) return depth+1;
+                        if(set.contains(newWord)){
+                            set.remove(newWord);
+                            queue.offer(newWord);
+                        }
+
                     }
                     arr[i]=temp;
                 }
             }
-
-
             depth++;
         }
 
 
-        return 0;
 
+        return 0;
 
     }
 }
