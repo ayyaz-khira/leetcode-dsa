caf082d89b0651166aa319f258034174940571bf
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-09-10 22:07:26 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (53.89%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -18,7 +18,7 @@ class Solution {
         if(p==null && q==null) return true;
 
         if(p==null || q==null) return false;
-
+        
         if(p.val==q.val && isSameTree(p.left,q.left) && isSameTree(p.right,q.right)) return true;
 
         return false;
