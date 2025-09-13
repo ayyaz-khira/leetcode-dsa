fe44282d1c333dd7bd1785ffd7099616c35eb34e
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-09-13 22:22:28 +0530

    Time: 12 ms (7.90%) | Memory: 46.8 MB (26.15%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,30 +1,24 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        List<int[]> res=new ArrayList<>();
-        Arrays.sort(intervals,new IntervalSort());
-        
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
 
-        for(int[] interval:intervals){
-            if(res.isEmpty() || res.get(res.size()-1)[1]<interval[0]){
-                res.add(interval);
+        List<int[]> list=new ArrayList<>();
+        for(int interval[]:intervals){
+            if(list.isEmpty() || list.get(list.size()-1)[1]<interval[0]){
+                list.add(interval);
             }
             else{
-                res.get(res.size()-1)[1]=Math.max(res.get(res.size()-1)[1],interval[1]);
+                list.get(list.size()-1)[1]=Math.max(list.get(list.size()-1)[1],interval[1]);
             }
         }
 
-        int arr[][]=new int[res.size()][];
-        for(int i=0;i<res.size();i++){
-            arr[i]=res.get(i);
+        int arr[][]=new int[list.size()][];
+        int j=0;
+        for(int x[]:list){
+            arr[j++]=x;
         }
 
-        return arr;
-        
-    }
-}
 
-class IntervalSort implements Comparator<int[]>{
-    public int compare(int arr1[],int arr2[]){
-        return arr1[0]-arr2[0];
+        return arr;
     }
 }
