2decc9fbb3656da42abb33885fdc93e9b4ed70a2
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-09-13 19:16:06 +0530

    Time: 1 ms (99.6%), Space: 44.9 MB (94.44%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,32 +1,27 @@
 class Solution {
     public int jump(int[] nums) {
         int n=nums.length;
-        int curMax=0;
-        int max=0;
         int jumps=0;
-
+        int maxReach=0;
+        int lastWindow=0;
         if(n==1) return 0;
-
-
         for(int i=0;i<n;i++){
-            max=Math.max(max,nums[i]+i);
+            maxReach=Math.max(maxReach,i+nums[i]);
 
-            if(i==curMax){
-                curMax=max;
+            if(i==lastWindow){
+                lastWindow=maxReach;
                 jumps++;
 
-
-                if(curMax>=n-1){
+                if(lastWindow>=n-1){
                     return jumps;
                 }
             }
 
-            
-        }
 
+        }
 
         return jumps;
 
-        
+
     }
 }
