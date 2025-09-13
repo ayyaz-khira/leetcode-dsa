0ef619bfb0eef68471ba374f315308e5c674a890
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-09-13 22:22:24 +0530

    Time: 12 ms (7.9%), Space: 46.8 MB (26.15%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,37 +1,24 @@
-class CustomComparator implements Comparator<int[]>{
-    public int compare(int a[],int b[]){
-        return Integer.compare(a[0],b[0]);
-    }
-}
-
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals,new CustomComparator());
-        int n=intervals.length;
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+
         List<int[]> list=new ArrayList<>();
-        for(int x[]:intervals){
-            if(list.isEmpty() || x[0]>list.get(list.size()-1)[1]){
-                list.add(x);
+        for(int interval[]:intervals){
+            if(list.isEmpty() || list.get(list.size()-1)[1]<interval[0]){
+                list.add(interval);
             }
             else{
-                list.get(list.size()-1)[1]=Math.max(list.get(list.size()-1)[1],x[1]);
+                list.get(list.size()-1)[1]=Math.max(list.get(list.size()-1)[1],interval[1]);
             }
         }
 
-
-        int result[][]=new int[list.size()][];
+        int arr[][]=new int[list.size()][];
         int j=0;
         for(int x[]:list){
-            result[j++]=x;
-
-
+            arr[j++]=x;
         }
 
 
-        return result;
-
-
-
-
+        return arr;
     }
 }
