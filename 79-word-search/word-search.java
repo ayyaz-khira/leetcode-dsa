981ec90ab04d569d28797386548763bc1b12fe7f
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-09-13 14:35:12 +0530

    Time: 144 ms (52.89%) | Memory: 41.9 MB (36.99%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,44 +1,37 @@
 class Solution {
-
-    public boolean solve(char mat[][],int row,int col,int rows,int cols,String word,int ind){
-
-        if(ind==word.length()) return true;
-        if(row<0 || row>=rows || col<0 || col>=cols || mat[row][col]!=word.charAt(ind)) return false;
-
-
-        char temp=mat[row][col];
-        mat[row][col]='#';
-
-        boolean found= solve(mat,row+1,col,rows,cols,word,ind+1) ||
-                       solve(mat,row-1,col,rows,cols,word,ind+1) ||
-                       solve(mat,row,col+1,rows,cols,word,ind+1) ||
-                       solve(mat,row,col-1,rows,cols,word,ind+1);
-
-
-        mat[row][col]=temp;
-
-        
-        return found;
-
-        
-        
-
-
-
-    }
-
-
     public boolean exist(char[][] board, String word) {
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
+        int rows=board.length;
+        int cols=board[0].length;
+
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
                 if(board[i][j]==word.charAt(0)){
-                    if(solve(board,i,j,board.length,board[0].length,word,0)) return true;
+                    if(solve(board,i,j,rows,cols,word,0)) return true;
                 }
             }
         }
 
 
-
         return false;
+
+    }
+
+    public boolean solve(char mat[][],int row,int col,int rows,int cols,String word,int ind){
+        if(ind==word.length()) return true;
+
+        if(row<0 || row>=rows || col<0 || col>=cols || mat[row][col]=='#' || mat[row][col]!=word.charAt(ind)) return false;
+
+        char temp=mat[row][col];
+        mat[row][col]='#';
+
+        boolean found=solve(mat,row+1,col,rows,cols,word,ind+1) ||
+                      solve(mat,row-1,col,rows,cols,word,ind+1) ||
+                      solve(mat,row,col+1,rows,cols,word,ind+1) ||
+                      solve(mat,row,col-1,rows,cols,word,ind+1);
+
+        mat[row][col]=temp;
+
+        return found;
+
     }
 }
