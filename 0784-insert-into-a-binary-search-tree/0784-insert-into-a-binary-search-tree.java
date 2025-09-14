c5bc596ee0ac3e9ac03da3ee87db03406f9aebbb
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-09-14 18:18:54 +0530

    Time: 0 ms (100%), Space: 45.2 MB (68.45%) - LeetHub

--- 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.java
+++ 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.java
@@ -19,15 +19,15 @@ class Solution {
             root=new TreeNode(val);
             return root;
         }
-        
+
         if(val > root.val){
             root.right=insertIntoBST(root.right,val);
         }
         else if(val < root.val){
             root.left=insertIntoBST(root.left,val);
         }
-        
+
+
         return root;
-        
     }
 }
