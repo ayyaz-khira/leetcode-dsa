e9a28912771a7440af092a64f55f773e598ea89a
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-09-14 14:21:37 +0530

    Time: 10 ms (80.43%), Space: 45.2 MB (40.34%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,33 +1,31 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        int n=s.length();
-        int freq[]=new int[26];
-        int slidingFreq[]=new int[26];
-        int k=p.length();
         List<Integer> res=new ArrayList<>();
-        int l=0;
-
+        int freq[]=new int[26];
+        int slidFreq[]=new int[26];
+        int k=p.length();
         for(char c:p.toCharArray()){
             freq[c-'a']++;
         }
+        int n=s.length();
+        int l=0;
 
         for(int r=0;r<n;r++){
             char c=s.charAt(r);
-            slidingFreq[c-'a']++;
 
-            if(Arrays.equals(freq,slidingFreq)){
+            slidFreq[c-'a']++;
+
+            if(Arrays.equals(freq,slidFreq)){
                 res.add(r-k+1);
             }
 
+
             if(r-l+1>=k){
-                slidingFreq[s.charAt(l)-'a']--;
+                slidFreq[s.charAt(l)-'a']--;
                 l++;
             }
         }
 
-
         return res;
-
-        
     }
 }
