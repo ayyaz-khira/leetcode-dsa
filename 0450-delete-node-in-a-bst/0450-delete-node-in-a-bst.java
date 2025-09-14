8d3848122e244f9d81b3c9ad4ae5569386b8c7ae
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-09-14 20:41:25 +0530

    Time: 0 ms (100%), Space: 46.3 MB (35.08%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -15,47 +15,40 @@
  */
 class Solution {
 
-    static int minValue(TreeNode root){
+    public int minValue(TreeNode root){
         while(root.left!=null){
             root=root.left;
         }
-        
         return root.val;
     }
 
 
 
-
-
     public TreeNode deleteNode(TreeNode root, int key) {
+        if(root==null) return null;
 
-        if(root==null){
-            return null;
-        }
-        
-        if(key > root.val){
+        if(key>root.val){
             root.right=deleteNode(root.right,key);
         }
-        else if(key < root.val){
+        else if(key<root.val){
             root.left=deleteNode(root.left,key);
-        }
-        else{
+        }else{
             if(root.left==null && root.right==null){
                 return null;
-            }
-            else if(root.right==null){
+            }else if(root.left==null){
+                return root.right;
+            }else if(root.right==null){
                 return root.left;
             }
-            else if(root.left==null){
-                return root.right;
-            }
             else{
-                root.val=minValue(root.right);
+                int minVal=minValue(root.right);
+                root.val=minVal;
                 root.right=deleteNode(root.right,root.val);
             }
         }
-        
+
+
+
         return root;
-        
     }
 }
