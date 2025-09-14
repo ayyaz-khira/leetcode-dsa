43930ad08e1cb3593b80f04d0350213278ee35fd
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-09-14 14:31:58 +0530

    Time: 98 ms (17.02%) | Memory: 45.7 MB (20.34%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,40 +1,38 @@
 class Solution {
     public String minWindow(String s, String t) {
         int n=s.length();
-        int minLen=Integer.MAX_VALUE;
-        int l=0;
-        int freq[]=new int[128];
-        String ans="";
+        int k=t.length();
         int count=0;
-
+        int freq[]=new int[128];
         for(char c:t.toCharArray()){
             freq[c]++;
         }
+        int minLen=Integer.MAX_VALUE;
+        String ans="";
+        int l=0;
 
         for(int r=0;r<n;r++){
             char c=s.charAt(r);
             freq[c]--;
-
             if(freq[c]>=0){
                 count++;
             }
 
-            while(count==t.length()){
+            while(count==k){
                 char leftChar=s.charAt(l);
                 freq[leftChar]++;
-
                 if(freq[leftChar]>0){
                     count--;
                 }
 
-                if(r-l+1<minLen){
+
+                if((r-l+1)<minLen){
                     minLen=r-l+1;
                     ans=s.substring(l,r+1);
                 }
+
                 l++;
             }
-
-
         }
 
         return ans;
