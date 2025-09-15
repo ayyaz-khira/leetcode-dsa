be4cf44dd3fbb3fbea74baac8dac2e2602ec308b
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-09-15 18:58:09 +0530

    Time: 6 ms (72.23%), Space: 45.9 MB (20.28%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,51 +1,50 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
-        Queue<Integer> queue=new LinkedList<>();
-        int count=0;
+        int indegree[]=new int[numCourses];
 
         for(int i=0;i<numCourses;i++){
             adj.add(new ArrayList<>());
         }
-        
+
+
         for(int x[]:prerequisites){
             int u=x[1];
             int v=x[0];
             adj.get(u).add(v);
         }
 
-        int indegree[]=new int[numCourses];
-        
-        
+
         for(int i=0;i<numCourses;i++){
             for(int x:adj.get(i)){
                 indegree[x]++;
             }
         }
 
+
+        Queue<Integer> queue=new LinkedList<>();
+
         for(int i=0;i<numCourses;i++){
             if(indegree[i]==0){
                 queue.offer(i);
             }
         }
+        int count=0;
 
         while(!queue.isEmpty()){
-            int node=queue.poll();
             count++;
-
+            int node=queue.poll();
             for(int x:adj.get(node)){
                 indegree[x]--;
                 if(indegree[x]==0){
                     queue.offer(x);
                 }
             }
+
         }
 
 
         return count==numCourses;
-
         
-
-
     }
 }
