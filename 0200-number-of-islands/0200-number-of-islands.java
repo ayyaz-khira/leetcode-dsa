d707cc0db01a28967ee17321c2e2600b3a0a1635
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-09-27 19:06:31 +0530

    Time: 2 ms (99.83%), Space: 49.7 MB (54.39%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,35 +1,32 @@
 class Solution {
+
+    public void dfs(char grid[][],int row,int col,int rows,int cols){
+        if(row<0 || row>=rows || col>=cols || col<0 || grid[row][col]=='0'){
+            return;
+        }
+
+        grid[row][col]='0';
+        
+        dfs(grid,row+1,col,rows,cols);
+        dfs(grid,row-1,col,rows,cols);
+        dfs(grid,row,col+1,rows,cols);
+        dfs(grid,row,col-1,rows,cols);
+    }
+
     public int numIslands(char[][] grid) {
         int rows=grid.length;
         int cols=grid[0].length;
-
-        int islands=0;
+        int count=0;
 
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
                 if(grid[i][j]=='1'){
                     dfs(grid,i,j,rows,cols);
-                    islands++;
+                    count++;
                 }
             }
         }
 
-
-
-        return islands;
-
-
-    }
-
-    public void dfs(char mat[][],int row,int col,int rows,int cols){
-        if(row<0 || row>=rows || col<0 || col>=cols || mat[row][col]=='0') return;
-
-        mat[row][col]='0';
-
-        dfs(mat,row+1,col,rows,cols);
-        dfs(mat,row-1,col,rows,cols);
-        dfs(mat,row,col-1,rows,cols);
-        dfs(mat,row,col+1,rows,cols);
-
+        return count;
     }
 }
