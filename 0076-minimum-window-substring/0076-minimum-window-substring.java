c090a255f910452a024ad0af07598d5d90a11a13
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-10-07 18:27:52 +0530

    Time: 100 ms (16.62%), Space: 45.9 MB (10.08%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,19 +1,22 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int n=s.length();
         int k=t.length();
-        int count=0;
+        int n=s.length();
+        String ans="";
+
         int freq[]=new int[128];
+
         for(char c:t.toCharArray()){
             freq[c]++;
         }
-        int minLen=Integer.MAX_VALUE;
-        String ans="";
-        int l=0;
 
+        int l=0;
+        int count=0;
+        int min=Integer.MAX_VALUE;
         for(int r=0;r<n;r++){
             char c=s.charAt(r);
             freq[c]--;
+
             if(freq[c]>=0){
                 count++;
             }
@@ -25,17 +28,18 @@ class Solution {
                     count--;
                 }
 
-
-                if((r-l+1)<minLen){
-                    minLen=r-l+1;
+                if((r-l+1)<min){
+                    min=r-l+1;
                     ans=s.substring(l,r+1);
                 }
-
                 l++;
+
             }
+
+
+            
         }
 
         return ans;
-
     }
 }
