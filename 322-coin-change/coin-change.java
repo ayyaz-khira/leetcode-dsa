4ba2c4e614533f4c9a72fa602a8c87ca51bd2640
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-10-07 22:55:05 +0530

    Time: 10 ms (99.72%) | Memory: 44.3 MB (82.05%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,7 +1,7 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int n=coins.length;
         int dp[]=new int[amount+1];
+        
         Arrays.fill(dp,amount+1);
         dp[0]=0;
 
@@ -11,6 +11,7 @@ class Solution {
             }
         }
 
-        return dp[amount]==amount+1? -1 : dp[amount];
+
+        return dp[amount]==amount+1?-1:dp[amount];
     }
 }
