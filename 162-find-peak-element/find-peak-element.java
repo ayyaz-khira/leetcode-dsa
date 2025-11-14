c7d7a29f73792870485892c5cd9621e678424f26
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-11-14 18:57:37 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (8.10%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -4,12 +4,14 @@ class Solution {
         if(n==1) return 0;
 
         if(nums[0]>nums[1]) return 0;
-        if(nums[n-1]>nums[n-2]) return n-1;
+        if(nums[n-2]<nums[n-1]) return n-1;
 
         for(int i=1;i<n-1;i++){
-            if(nums[i]>nums[i-1] && nums[i]>nums[i+1]) return i;
+            if(nums[i-1]<nums[i] && nums[i]>nums[i+1]){
+                return i;
+            }
         }
 
-        return 0;
+        return -1;
     }
 }
