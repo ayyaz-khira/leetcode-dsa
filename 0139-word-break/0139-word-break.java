a4f68a898d542559a1fb21fee6a848a151407545
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2025-12-01 20:19:47 +0530

    Time: 5 ms (63.07%), Space: 46.3 MB (12.6%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -11,7 +11,7 @@ class Solution {
         dp[0]=true;
 
         for(int i=0;i<=n;i++){
-            for(int j=i;j>=0;j--){
+            for(int j=i-1;j>=0;j--){
                 if(set.contains(s.substring(j,i)) && dp[j]){
                     dp[i]=true;
                     break;
