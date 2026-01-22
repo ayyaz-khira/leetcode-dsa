3f5e9c761f21da3a1f59ea8f727db78ee81cc45d
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2026-01-22 21:42:12 +0530

    Time: 3 ms (75.87%), Space: 51.3 MB (88.51%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,17 +1,38 @@
 class Solution {
-    public List<Integer> majorityElement(int[] nums) {
-        int n=nums.length;
+    public List<Integer> majorityElement(int[] arr) {
         List<Integer> list=new ArrayList<>();
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<n;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
-
-        for(var x:map.entrySet()){
-            if(x.getValue()>n/3){
-                list.add(x.getKey());
+        int n=arr.length;
+        int count1=0;
+        int count2=0;
+        int element1=-1;
+        int element2=-1;
+        for(int x:arr){
+            if(count1==0 && x!=element2){
+                element1=x;
+                count1++;
+            }
+            else if(count2==0 && x!=element1){
+                element2=x;
+                count2++;
+            }
+            else if(x==element1) count1++;
+            else if(x==element2) count2++;
+            else{
+                count1--;
+                count2--;
             }
         }
+
+        count1 = 0;
+        count2 = 0;
+
+        for (int x : arr) {
+            if (x == element1) count1++;
+            else if (x == element2) count2++;
+        }
+
+        if (count1 > n / 3) list.add(element1);
+        if (count2 > n / 3) list.add(element2);
 
         return list;
     }
