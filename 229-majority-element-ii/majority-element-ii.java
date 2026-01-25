7e967c281d1fe487f98f3c72896530901226fef6
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2026-01-25 10:50:56 +0530

    Time: 2 ms (99.64%) | Memory: 52.6 MB (44.55%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,38 +1,48 @@
 class Solution {
-    public List<Integer> majorityElement(int[] arr) {
-        List<Integer> list=new ArrayList<>();
-        int n=arr.length;
-        int count1=0;
-        int count2=0;
+    public List<Integer> majorityElement(int[] nums) {
+        int n=nums.length;
         int element1=-1;
         int element2=-1;
-        for(int x:arr){
+        int count1=0;
+        int count2=0;
+
+        List<Integer> list=new ArrayList<>();
+
+        for(int x:nums){
             if(count1==0 && x!=element2){
                 element1=x;
-                count1++;
+                count1=1;
             }
             else if(count2==0 && x!=element1){
                 element2=x;
+                count2=1;
+            }
+            else if(x==element1){
+                count1++;
+            }
+            else if(x==element2){
                 count2++;
             }
-            else if(x==element1) count1++;
-            else if(x==element2) count2++;
             else{
                 count1--;
                 count2--;
             }
         }
 
-        count1 = 0;
-        count2 = 0;
+        count1=0;
+        count2=0;
 
-        for (int x : arr) {
-            if (x == element1) count1++;
-            else if (x == element2) count2++;
+        for(int x:nums){
+            if(x==element1){
+                count1++;
+            }
+            else if(x==element2){
+                count2++;
+            }
         }
 
-        if (count1 > n / 3) list.add(element1);
-        if (count2 > n / 3) list.add(element2);
+        if(count1>n/3) list.add(element1);
+        if(count2>n/3) list.add(element2);
 
         return list;
     }
