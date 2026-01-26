82db6d196fe52c5191d93c436fe9f7ba180cc8a6
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2026-01-26 11:36:27 +0530

    Time: 0 ms (100%), Space: 47.6 MB (26.96%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,21 +1,17 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n=nums.length;
-        int  k=0;
-        for(int i=0;i<=n;i++){
-            int flag=0;
-            for(int j=0;j<n;j++){
-                if(nums[j]==i){
-                    flag=1;
-                }
-            }
 
-            if(flag==0){
-                k=i;
-            }
+        int ans=0;
+
+        for(int i=0;i<n;i++){
+            ans=ans^i;
+            ans=ans^nums[i];
 
         }
 
-        return k;
+        ans=ans^n;
+
+        return ans;
     }
 }
