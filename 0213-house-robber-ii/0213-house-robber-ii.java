de30c6fa54e06058e39d987edd24d62af14c6845
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2026-02-04 21:51:27 +0530

    Time: 0 ms (100%), Space: 43.1 MB (6.3%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,35 +1,32 @@
 class Solution {
+
+    public int robber(int arr[]){
+        int n=arr.length;
+        if(n==0) return 0;
+        if(n==1) return arr[0];
+        if(n==2) return Math.max(arr[0],arr[1]);
+        
+        int dp[]=new int[n];
+        dp[0]=arr[0];
+        dp[1]=Math.max(arr[0],arr[1]);
+        for(int i=2;i<n;i++){
+            dp[i]=Math.max(dp[i-1],dp[i-2]+arr[i]);
+        }
+        
+        return dp[n-1];
+    }
+
     public int rob(int[] nums) {
         int n=nums.length;
         if(n==0) return 0;
         if(n==1) return nums[0];
         if(n==2) return Math.max(nums[0],nums[1]);
+        int max1=robber(Arrays.copyOfRange(nums,0,n-1));
+        int max2=robber(Arrays.copyOfRange(nums,1,n));
 
-        int firstHalf=robber(Arrays.copyOfRange(nums,0,n-1));
-        int secondHalf=robber(Arrays.copyOfRange(nums,1,n));
-
-        return Math.max(firstHalf,secondHalf);
+        return Math.max(max1,max2);
 
         
-    }
 
-    public int robber(int nums[]){
-        int n=nums.length;
-
-        if(n==0) return 0;
-        if(n==1) return nums[0];
-        if(n==2) return Math.max(nums[0],nums[1]);
-        
-        int dp[]=new int[n];
-
-        dp[0]=nums[0];
-        dp[1]=Math.max(nums[0],nums[1]);
-
-        for(int i=2;i<n;i++){
-            dp[i]=Math.max(dp[i-1],dp[i-2]+nums[i]);
-        }
-
-
-        return dp[n-1];
     }
 }
