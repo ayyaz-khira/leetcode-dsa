e1090470c65e111b307bda574643ff839fbdd2b7
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2026-02-08 20:53:13 +0530

    Time: 6 ms (72.89%), Space: 45.5 MB (26.78%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        int n=candidates.length;
+        List<List<Integer>> res=new ArrayList<>();
+        List<Integer> comb=new ArrayList<>();
+        recurse(candidates,0,target,res,comb);
+
+        return new ArrayList<>(res);
+    }
+
+    public void recurse(int arr[],int ind,int target,List<List<Integer>> res,List<Integer> comb){
+        if(target==0){
+            res.add(new ArrayList<>(comb));
+            return;
+        }
+
+        for(int i=ind;i<arr.length;i++){
+        
+        if(i>ind && arr[i]==arr[i-1]) continue;
+        if(arr[i]>target) break;        
+
+        comb.add(arr[i]);
+        recurse(arr,i+1,target-arr[i],res,comb);
+        comb.remove(comb.size()-1);
+
+
+        }
+
+
+    }
+}
