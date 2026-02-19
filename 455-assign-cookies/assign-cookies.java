8710715035aa551f645951c71326faa9fc6e3805
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2026-02-19 13:35:55 +0530

    Time: 14 ms (92.91%) | Memory: 51.6 MB (36.62%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int i=0;
+        int j=0;
+        Arrays.sort(s);
+        Arrays.sort(g);
+        int count=0;
+        while(i<g.length && j<s.length){
+            if(s[j]>=g[i]){
+                count++;
+
+            i++;
+            j++;
+            }
+            else{
+                j++;
+            }
+
+
+        }
+
+        return count;
+    }
+}
