f147b342a72be41d73d3bf9a876823c4b28c74cd
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2026-03-24 22:22:59 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (87.97%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,28 +1,31 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] grid) {
-         if(grid[0][0]==1) return 0;
-        int rows=grid.length;
-        int cols=grid[0].length;
-        
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int rows=obstacleGrid.length;
+        int cols=obstacleGrid[0].length;
+
+        if(obstacleGrid[0][0]==1) return 0;
 
         int dp[][]=new int[rows][cols];
+
         dp[0][0]=1;
-        
+
+
         for(int i=1;i<rows;i++){
-            if(grid[i][0]==0){
-            dp[i][0]=dp[i-1][0];
+            if(obstacleGrid[i][0]==0){
+                dp[i][0]=dp[i-1][0];
             }
         }
-        
+
         for(int j=1;j<cols;j++){
-            if(grid[0][j]==0){
-            dp[0][j]=dp[0][j-1];
+            if(obstacleGrid[0][j]==0){
+                dp[0][j]=dp[0][j-1];
             }
         }
-        
+
+
         for(int i=1;i<rows;i++){
             for(int j=1;j<cols;j++){
-                if(grid[i][j]==1){
+                if(obstacleGrid[i][j]==1){
                     dp[i][j]=0;
                 }
                 else{
@@ -30,6 +33,9 @@ class Solution {
                 }
             }
         }
+
+
         return dp[rows-1][cols-1];
+
     }
 }
