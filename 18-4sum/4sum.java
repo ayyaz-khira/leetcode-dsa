22b1f03e48bc604e045eae7f15bc0e80ba3b7a6d
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2026-05-19 16:15:22 +0530

    Time: 20 ms (66.22%) | Memory: 46 MB (30.80%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,36 +1,41 @@
 class Solution {
-    public List<List<Integer>> fourSum(int[] nums, int target) {
-        Arrays.sort(nums);
+    public List<List<Integer>> fourSum(int[] arr, int target) {
         List<List<Integer>> res=new ArrayList<>();
-        int n=nums.length;
-        for(int i=0;i<n-3;i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
-            int first=nums[i];
-            for(int j=i+1;j<n-2;j++){
-                if(j>i+1 && nums[j]==nums[j-1]) continue;
-                int second=nums[j];
-                int left=j+1;
-                int right=n-1;
+        int n=arr.length;
+        Arrays.sort(arr);
 
-                while(left<right){
-                    long total=(long)first+second+nums[left]+nums[right];
-                    if(total==target){
-                        List<Integer> list=new ArrayList<>(Arrays.asList(first,second,nums[left],nums[right]));
+        for(int k=0;k<n;k++){
+            if(k>0 && arr[k]==arr[k-1]) continue;
+            int firstEl=arr[k];
+            for(int m=k+1;m<n-1;m++){
+                if(m>k+1 && arr[m]==arr[m-1]) continue;
+                int secondEl=arr[m];
+                int third=m+1;
+                int fourth=n-1;
+
+                while(third<fourth){
+                    long sum=(long)firstEl+secondEl+arr[third]+arr[fourth];
+                    if(sum==target){
+                        List<Integer> list=new ArrayList<>(Arrays.asList(firstEl,secondEl,arr[third],arr[fourth]));
                         res.add(list);
-                        left++;
-                        right--;
-                        while(left<right && nums[left]==nums[left-1]) left++;
-                        while(left<right && nums[right]==nums[right+1]) right--;
-                    
+
+                        third++;
+                        fourth--;
+
+                        while(third<fourth && arr[third]==arr[third-1]) third++;
+                        while(third<fourth && arr[fourth]==arr[fourth+1]) fourth-- ;
+
                     }
-                    else if(total<target){
-                        left++;
+                    else if(sum<target){
+                        third++;
                     }
                     else{
-                        right--;
+                        fourth--;
                     }
                 }
             }
+           
+
         }
 
 
