4957262ea160a04bfb69e5cfa37d87010deb1d32
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2026-05-26 14:47:15 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (8.30%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -1,53 +1,49 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> res=new ArrayList<>();
-        int m=matrix.length;
-        int n=matrix[0].length;
-        int srow=0;
         int scol=0;
-        int ecol=n-1;
-        int erow=m-1;
+        int srow=0;
+        int ecol=matrix[0].length-1;
+        int erow=matrix.length-1;
+        List<Integer> res=new ArrayList<>();
+
 
         while(srow<=erow && scol<=ecol){
-
+            
             //top
             for(int j=scol;j<=ecol;j++){
                 res.add(matrix[srow][j]);
             }
+
 
             //right
             for(int i=srow+1;i<=erow;i++){
                 res.add(matrix[i][ecol]);
             }
 
+
             //bottom
-            if(srow==erow){
-                break;
-            }else{
+            if(srow!=erow){
             for(int j=ecol-1;j>=scol;j--){
                 res.add(matrix[erow][j]);
             }
             }
 
+
             //left
-            if(scol==ecol){
-                break;
-            }
-            else{
+            if(scol!=ecol){
             for(int i=erow-1;i>=srow+1;i--){
                 res.add(matrix[i][scol]);
             }
             }
 
-            srow++;
+
             scol++;
+            srow++;
             erow--;
             ecol--;
         }
 
 
         return res;
-        
-        
     }
 }
