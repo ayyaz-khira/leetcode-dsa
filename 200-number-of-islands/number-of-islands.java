76cdc2973da201a8635a9b1ee9a846e16e6aa705
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2026-05-27 12:24:40 +0530

    Time: 2 ms (99.79%) | Memory: 52.4 MB (33.20%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,23 +1,24 @@
 class Solution {
 
-    public void dfs(char grid[][],int row,int col,int rows,int cols){
-        if(row<0 || row>=rows || col>=cols || col<0 || grid[row][col]=='0'){
-            return;
-        }
+    public void dfs(char mat[][], int row, int col, int rows, int cols){
+        if(row<0 || row>=rows || col<0 || col>=cols || mat[row][col]=='0') return;
 
-        grid[row][col]='0';
-        
-        dfs(grid,row+1,col,rows,cols);
-        dfs(grid,row-1,col,rows,cols);
-        dfs(grid,row,col+1,rows,cols);
-        dfs(grid,row,col-1,rows,cols);
+        mat[row][col]='0';
+
+        dfs(mat,row+1,col,rows,cols);
+        dfs(mat,row-1,col,rows,cols);
+        dfs(mat,row,col+1,rows,cols);
+        dfs(mat,row,col-1,rows,cols);
+
+
+
     }
+
 
     public int numIslands(char[][] grid) {
         int rows=grid.length;
         int cols=grid[0].length;
         int count=0;
-
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
                 if(grid[i][j]=='1'){
@@ -27,6 +28,8 @@ class Solution {
             }
         }
 
+
         return count;
+
     }
 }
