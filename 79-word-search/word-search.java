514a5744792398d58ba97a31827057ed9a14498a
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2026-05-27 12:14:23 +0530

    Time: 143 ms (46.60%) | Memory: 43.1 MB (29.84%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,45 +1,39 @@
 class Solution {
 
-    public boolean isSafe(char mat[][],int i,int j,int rows,int cols,String word,int ind){
-        if(ind==word.length()) return true;
+    public boolean found(char mat[][], int row, int col, int rows, int cols, String word, int ind){
+        if(word.length()==ind) return true;
+
+        if(row<0 || row>=rows || col<0 || col>=cols || mat[row][col]=='#' || mat[row][col]!=word.charAt(ind)){
+            return false;
+        }
+
+        char temp=mat[row][col];
+        mat[row][col]='#';
+
+        boolean isFound= found(mat, row+1, col, rows, cols, word, ind+1)
+                    ||   found(mat, row-1, col, rows, cols, word, ind+1)
+                    ||   found(mat, row, col+1, rows, cols, word, ind+1)
+                    ||   found(mat, row, col-1, rows, cols, word, ind+1);
+
+        mat[row][col]=temp;
+        
+        
+        return isFound;
 
 
-        if(i<0 || i>=rows || j<0 || j>=cols || mat[i][j]=='#' || word.charAt(ind)!=mat[i][j]) return false;
-
-        char temp=mat[i][j];
-
-        mat[i][j]='#';
-
-        boolean found=isSafe(mat,i+1,j,rows,cols,word,ind+1) ||
-        isSafe(mat,i-1,j,rows,cols,word,ind+1) ||
-        isSafe(mat,i,j+1,rows,cols,word,ind+1) ||
-        isSafe(mat,i,j-1,rows,cols,word,ind+1);
-
-        mat[i][j]=temp;
-
-        return found;
-
-
-
+        
     }
 
 
-
-
-
-
     public boolean exist(char[][] board, String word) {
-
         int rows=board.length;
         int cols=board[0].length;
-
+        boolean vis[][]=new boolean[rows][cols];
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
-               if(board[i][j]==word.charAt(0)){
-                if(isSafe(board,i,j,rows,cols,word,0)){
-                    return true;
+                if(word.charAt(0)==board[i][j]){
+                    if(found(board,i,j,rows,cols,word,0)) return true;
                 }
-               }
             }
         }
 
