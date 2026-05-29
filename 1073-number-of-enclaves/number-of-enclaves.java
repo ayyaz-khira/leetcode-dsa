2d65a00420187c213ab07eeb4a8c681165a93300
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2026-05-29 11:04:12 +0530

    Time: 7 ms (84.78%) | Memory: 62.6 MB (17.00%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,53 +1,50 @@
 class Solution {
 
-    public int dfs(int grid[][],int i,int j,int rows,int cols){
-        if(i<0 || j<0 || i>=rows || j>=cols || grid[i][j]==0) return 0;
+    public void dfs(int grid[][], int rows, int cols, int row, int col){
+        if(row<0 || row>=rows || col<0 || col>=cols || grid[row][col]==0) return;
 
-        int count=0;
+        grid[row][col]=0;
 
-        grid[i][j]=0;
-        count+=1;
+        dfs(grid, rows, cols, row+1, col);
+        dfs(grid, rows, cols, row-1, col);
+        dfs(grid, rows, cols, row, col+1);
+        dfs(grid, rows, cols, row, col-1);
 
-        count+=dfs(grid,i+1,j,rows,cols);
-        count+=dfs(grid,i-1,j,rows,cols);
-        count+=dfs(grid,i,j+1,rows,cols);
-        count+=dfs(grid,i,j-1,rows,cols);
-
-
-        return count;
-       
     }
 
+
+
+
     public int numEnclaves(int[][] grid) {
-        int count=0;
         int rows=grid.length;
         int cols=grid[0].length;
-        int ones=0;
+
+        int land=0;
+
+
+        for(int i=0;i<rows;i++){
+            dfs(grid,rows,cols,i,0);
+            dfs(grid,rows,cols,i,cols-1);
+
+        }
+
+        for(int j=0;j<cols;j++){
+            dfs(grid,rows,cols,0,j);
+            dfs(grid,rows,cols,rows-1,j);
+        }
+
 
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
-                if(grid[i][j]==1) ones++;
+                if(grid[i][j]==1){
+                    land++;
+                }
             }
         }
 
 
-        for(int i=0;i<rows;i++){
-            count+=dfs(grid,i,0,rows,cols);
-        }
+        return land;
 
-        for(int j=0;j<cols;j++){
-            count+=dfs(grid,0,j,rows,cols);
-        }
-
-        for(int i=0;i<rows;i++){
-            count+=dfs(grid,i,cols-1,rows,cols);
-        }
-        for(int j=0;j<cols;j++){
-            count+=dfs(grid,rows-1,j,rows,cols);
-        }
-
-
-        return ones-count;
 
 
     }
