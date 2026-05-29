49f459a679601c6fd0b93a7910d33a1acd3dcb68
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2026-05-29 14:33:55 +0530

    Time: 26 ms (16.43%) | Memory: 47.8 MB (34.37%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,75 +1,55 @@
 class Solution {
 
-    public boolean isSafe(int grid[][],int i,int j,int rows,int cols){
-        if(i<0 || i>=rows || j<0 || j>=cols || grid[i][j]==1){
-            return false;
-        }else{
-           return true;
+    public void perform(Queue queue,int grid[][], int i, int j, int rows, int cols){
+        if(i>=0 && i<rows && j>=0 && j<cols && grid[i][j]==0){
+            queue.add(new int[]{i,j});
+            grid[i][j]=1;
         }
     }
 
-    public void modify(Queue queue,int grid[][],int i,int j){
-        grid[i][j]=1;
-        queue.add(new int[]{i,j});
-    }
 
 
     public int shortestPathBinaryMatrix(int[][] grid) {
-        int levels=1;
-        Queue<int[]> queue=new LinkedList<>();
-        int cols=grid[0].length;
         int rows=grid.length;
+        int cols=grid[0].length;
 
-        if(grid[0][0]==1){
-            return -1;
-        }
-        else{
+        if(grid[0][0]==1) return -1;
+
+        Queue<int[]> queue=new LinkedList<>();
+
         queue.add(new int[]{0,0});
-        grid[0][0]=1;
-        }
+
+        int level=1;
 
         while(!queue.isEmpty()){
             int size=queue.size();
 
+            
+
             for(int k=0;k<size;k++){
                 
-                int[] poll=queue.poll();
-                int i=poll[0];
-                int j=poll[1];
+                int res[]=queue.poll();
+                int i=res[0];
+                int j=res[1];
 
-                if(i==rows-1 && j==cols-1) return levels;
+                if(i==rows-1 && j==cols-1) return level;
 
-                if(isSafe(grid,i+1,j,rows,cols)){
-                    modify(queue,grid,i+1,j);
-                }
-                if(isSafe(grid,i-1,j,rows,cols)){
-                    modify(queue,grid,i-1,j);
-                }
-                if(isSafe(grid,i,j-1,rows,cols)){
-                    modify(queue,grid,i,j-1);
-                }
-                if(isSafe(grid,i,j+1,rows,cols)){
-                    modify(queue,grid,i,j+1);
-                }
+                perform(queue,grid,i+1,j,rows,cols);
+                perform(queue,grid,i-1,j,rows,cols);
+                perform(queue,grid,i,j+1,rows,cols);
+                perform(queue,grid,i,j-1,rows,cols);
+                perform(queue,grid,i+1,j+1,rows,cols);
+                perform(queue,grid,i-1,j-1,rows,cols);
+                perform(queue,grid,i+1,j-1,rows,cols);
+                perform(queue,grid,i-1,j+1,rows,cols);
 
-                if(isSafe(grid,i-1,j-1,rows,cols)){
-                    modify(queue,grid,i-1,j-1);
-                }
-                if(isSafe(grid,i+1,j+1,rows,cols)){
-                    modify(queue,grid,i+1,j+1);
-                }
-                if(isSafe(grid,i+1,j-1,rows,cols)){
-                    modify(queue,grid,i+1,j-1);
-                }
-                if(isSafe(grid,i-1,j+1,rows,cols)){
-                    modify(queue,grid,i-1,j+1);
-                }
 
-                
             }
 
-            levels+=1;
+            level++;
+
         }
+
 
         return -1;
     }
