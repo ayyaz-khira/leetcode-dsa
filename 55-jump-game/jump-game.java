beb3c8abac92ee0bd07768f93b9bd469a4c0c560
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2026-06-06 14:17:08 +0530

    Time: 2 ms (88.03%) | Memory: 47.9 MB (60.67%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,16 +1,16 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int n=nums.length;
-        int farthest=n-1;
-
+        int lastInd=n-1;
         for(int i=n-2;i>=0;i--){
-            if(farthest-i<=nums[i]){
-                farthest=i;
+            if(nums[i]>=lastInd-i){
+                lastInd=i;
             }
         }
 
-        if(farthest==0) return true;
-        
+
+        if(lastInd==0) return true;
+
         return false;
     }
 }
