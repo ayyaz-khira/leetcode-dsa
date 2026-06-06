4f60b5762e82938f998af6b5a9212c953db8ac23
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2026-06-06 10:27:56 +0530

    Time: 2 ms (99.92%) | Memory: 52.6 MB (51.11%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,48 +1,51 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int n=nums.length;
-        int element1=-1;
-        int element2=-1;
+        List<Integer> list=new ArrayList<>();
+        int candidate1=0;
+        int candidate2=0;
         int count1=0;
         int count2=0;
-
-        List<Integer> list=new ArrayList<>();
+        int n=nums.length;
 
         for(int x:nums){
-            if(count1==0 && x!=element2){
-                element1=x;
-                count1=1;
-            }
-            else if(count2==0 && x!=element1){
-                element2=x;
-                count2=1;
-            }
-            else if(x==element1){
+            if(x==candidate1){
                 count1++;
             }
-            else if(x==element2){
+            else if(x==candidate2){
                 count2++;
+            }
+            else if(count1==0){
+                candidate1=x;
+                count1=1;
+            }
+            else if(count2==0){
+                candidate2=x;
+                count2=1;
             }
             else{
                 count1--;
                 count2--;
             }
+
+
         }
 
         count1=0;
         count2=0;
 
         for(int x:nums){
-            if(x==element1){
-                count1++;
-            }
-            else if(x==element2){
-                count2++;
-            }
+            if(x==candidate1) count1++;
+            if(x==candidate2) count2++;
+
         }
 
-        if(count1>n/3) list.add(element1);
-        if(count2>n/3) list.add(element2);
+
+        if(count1>n/3) list.add(candidate1);
+
+
+        if(count2 > n/3 && candidate2 != candidate1)
+        list.add(candidate2);
+
 
         return list;
     }
