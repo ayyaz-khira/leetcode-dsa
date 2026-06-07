da5c03d66d5fad5a0471f11d1591bd50d65c9b7c
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2026-06-07 15:29:48 +0530

    Time: 14 ms (55.32%), Space: 52.3 MB (5.29%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,32 +1,33 @@
 class Solution {
     public String frequencySort(String s) {
         int n=s.length();
+
         List<Character>[] bucket=new ArrayList[n+1];
         HashMap<Character,Integer> map=new HashMap<>();
-        StringBuilder res=new StringBuilder();
-        
+        String res="";
 
         for(int i=0;i<bucket.length;i++){
             bucket[i]=new ArrayList<>();
         }
-        for(char c:s.toCharArray()){
-            map.put(c,map.getOrDefault(c,0)+1);
+
+        for(char x:s.toCharArray()){
+            map.put(x,map.getOrDefault(x,0)+1);
         }
 
         for(var x:map.entrySet()){
             char key=x.getKey();
             int freq=x.getValue();
+
             bucket[freq].add(key);
         }
 
-        for(int i=bucket.length-1;i>=0 && n>0;i--){
-            for(char x:bucket[i]){
-                res.append(String.valueOf(x).repeat(i));
-                n--;
+        for(int i=bucket.length-1;i>=0;i--){
+            for(char c:bucket[i]){
+                res+=String.valueOf(c).repeat(i);
             }
         }
-    return res.toString();
+
+
+        return res;
     }
-
-
 }
