0308688f731f685d42e50f7dd15667ed949dbd9e
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2026-06-07 15:29:51 +0530

    Time: 14 ms (55.32%) | Memory: 52.3 MB (5.29%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,8 +1,10 @@
 class Solution {
     public String frequencySort(String s) {
         int n=s.length();
+
+        List<Character>[] bucket=new ArrayList[n+1];
         HashMap<Character,Integer> map=new HashMap<>();
-        List<Character> bucket[]=new ArrayList[n+1];
+        String res="";
 
         for(int i=0;i<bucket.length;i++){
             bucket[i]=new ArrayList<>();
@@ -15,15 +17,16 @@ class Solution {
         for(var x:map.entrySet()){
             char key=x.getKey();
             int freq=x.getValue();
+
             bucket[freq].add(key);
         }
-        String res="";
+
         for(int i=bucket.length-1;i>=0;i--){
             for(char c:bucket[i]){
                 res+=String.valueOf(c).repeat(i);
-                n--;
             }
         }
+
 
         return res;
     }
