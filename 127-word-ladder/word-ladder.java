dea5f80dd1e96ba6c4ee7d7cfd3644ac76440b50
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2026-06-09 11:48:00 +0530

    Time: 76 ms (55.15%) | Memory: 47.9 MB (58.80%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,37 +1,46 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        if(!wordList.contains(endWord)) return 0;
         HashSet<String> set=new HashSet<>();
         for(String x:wordList){
             set.add(x);
         }
+        if(!set.contains(endWord)) return 0;
 
         Queue<String> queue=new LinkedList<>();
         queue.add(beginWord);
-        int count=1;
-
+        int level=1;
         while(!queue.isEmpty()){
             int size=queue.size();
 
-            for(int i=0;i<size;i++){
-                String word=queue.poll();
+            
+            for(int k=0;k<size;k++){
+
+            String word=queue.poll();
+            for(int i=0;i<word.length();i++){
                 char arr[]=word.toCharArray();
-                for(int j=0;j<arr.length;j++){
-                    char temp=arr[j];
-                    for(char c='a';c<='z';c++){
-                        arr[j]=c;
-                        String newWord=String.valueOf(arr);
-                        if(newWord.equals(endWord)) return count+1;
-                        if(set.contains(newWord)) {queue.add(newWord); set.remove(newWord);}
-                        
+                char temp=arr[i];
+
+                for(char c='a';c<='z';c++){
+                    if(c==temp) continue;
+                    arr[i]=c;
+
+                    String newWord=new String(arr);
+
+                    if(newWord.equals(endWord)) return level+1;
+                    if(set.contains(newWord)){
+                        queue.add(newWord);
+                        set.remove(newWord);
                     }
-                    arr[j]=temp;
                 }
 
+                arr[i]=temp;
             }
-            count+=1;
+            }
+
+            level+=1;
         }
 
-        return 0;
+
+        return 0;        
     }
 }
