dbb28161d34250b53e7bbce08213223be6aa8954
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2026-06-09 10:12:23 +0530

    Time: 7 ms (58.77%) | Memory: 47.4 MB (12.34%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,17 +1,21 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+
         for(int i=0;i<numCourses;i++){
             adj.add(new ArrayList<>());
         }
 
+
         for(int x[]:prerequisites){
             int u=x[1];
             int v=x[0];
+
             adj.get(u).add(v);
         }
 
         int indegree[]=new int[numCourses];
+
         for(int i=0;i<numCourses;i++){
             for(int x:adj.get(i)){
                 indegree[x]++;
@@ -19,24 +23,35 @@ class Solution {
         }
 
         Queue<Integer> queue=new LinkedList<>();
+
+        
         for(int i=0;i<numCourses;i++){
             if(indegree[i]==0){
-                queue.offer(i);
+            queue.add(i);
             }
         }
+
 
         int count=0;
+
         while(!queue.isEmpty()){
-            count++;
-            int node=queue.poll();
-            for(int x:adj.get(node)){
-                indegree[x]--;
-                if(indegree[x]==0){
-                    queue.offer(x);
+            
+            int x=queue.poll();
+
+            for(int y:adj.get(x)){
+                indegree[y]-=1;
+                if(indegree[y]==0){
+                    queue.add(y);
                 }
+
             }
+
+            count+=1;
         }
 
-        return numCourses==count;
+
+        return count==numCourses;
+
+
     }
 }
