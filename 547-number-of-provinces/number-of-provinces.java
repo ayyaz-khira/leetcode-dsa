3491e9cf367d11fae8592866a4867da6c7b21676
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2026-06-12 11:16:06 +0530

    Time: 3 ms (27.95%) | Memory: 49.6 MB (10.79%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,44 +1,48 @@
 class Solution {
 
-    public void dfs(int node,boolean vis[],ArrayList<ArrayList<Integer>> adj){
+    public void dfs(ArrayList<ArrayList<Integer>> adj, int node, boolean vis[]){
         vis[node]=true;
 
         for(int x:adj.get(node)){
             if(!vis[x]){
-                dfs(x,vis,adj);
+                dfs(adj,x,vis);
+                vis[x]=true;
             }
         }
     }
 
 
     public int findCircleNum(int[][] isConnected) {
+        int n=isConnected.length;
+
+        int total=0;
+
         ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
 
-        for(int i=0;i<isConnected.length;i++){
+        for(int i=0;i<n;i++){
             adj.add(new ArrayList<>());
         }
 
-        for(int i=0;i<isConnected.length;i++){
-            for(int j=0;j<isConnected[0].length;j++){
-                if(i!=j && isConnected[i][j]==1){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1){
                     adj.get(i).add(j);
                 }
+                
             }
         }
 
         boolean vis[]=new boolean[adj.size()];
-    int provinces=0;
-    for(int i=0;i<adj.size();i++){
-        if(!vis[i]){
-            dfs(i,vis,adj);
-            provinces++;
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+            dfs(adj,i,vis);
+            total+=1;
         }
+        }
+
+
+        return total;
+
+
     }
-
-    return provinces;
-    }
-
-
-    
-
 }
