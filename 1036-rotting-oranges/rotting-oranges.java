b2023e6426b0279d24c18a68e842f43250720d7a
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2026-06-13 11:59:14 +0530

    Time: 1 ms (100.00%) | Memory: 44.1 MB (55.82%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,10 +1,15 @@
 class Solution {
+
+
     public int orangesRotting(int[][] grid) {
-        Queue<int[]> queue=new LinkedList<>();
+
+
         int rows=grid.length;
         int cols=grid[0].length;
+
+        Queue<int[]> queue=new LinkedList<>();
+        int level=0;
         int fresh=0;
-        int rotten=0;
 
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
@@ -20,47 +25,53 @@ class Solution {
 
         if(fresh==0) return 0;
 
-        int count=0;
-
         while(!queue.isEmpty()){
             int size=queue.size();
 
             for(int k=0;k<size;k++){
-                
                 int poll[]=queue.poll();
                 int i=poll[0];
                 int j=poll[1];
 
                 if(i+1<rows && grid[i+1][j]==1){
-                    grid[i+1][j]=2;
-                    queue.add(new int[]{i+1,j});
-                    fresh--;
+
+        queue.add(new int[]{i+1,j});
+        grid[i+1][j]=2;
+        fresh--;
                 }
 
                 if(i-1>=0 && grid[i-1][j]==1){
-                    grid[i-1][j]=2;
-                    queue.add(new int[]{i-1,j});
-                    fresh--;
 
+        queue.add(new int[]{i-1,j});
+        grid[i-1][j]=2;
+        fresh--;
                 }
 
                 if(j+1<cols && grid[i][j+1]==1){
-                    grid[i][j+1]=2;
-                    queue.add(new int[]{i,j+1});
-                    fresh--;
+
+        queue.add(new int[]{i,j+1});
+        grid[i][j+1]=2;
+        fresh--;
                 }
 
                 if(j-1>=0 && grid[i][j-1]==1){
-                    grid[i][j-1]=2;
-                    queue.add(new int[]{i,j-1});
-                    fresh--;
+
+        queue.add(new int[]{i,j-1});
+        grid[i][j-1]=2;
+        fresh--;
                 }
+
+
+                
+                
             }
 
-            count++;
-        }
-        
 
-        return fresh==0? count-1 : -1;
+            level++;
+        }
+
+
+
+        return fresh==0 ? level-1 : -1;
     }
 }
