0de0ef2bb02ef4f75a549a841170c8dda6c452a8
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2026-06-13 11:16:10 +0530

    Time: 15 ms (42.28%) | Memory: 49.3 MB (40.54%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,23 +1,24 @@
 class Solution {
-
-
     public int[][] updateMatrix(int[][] mat) {
         int rows=mat.length;
         int cols=mat[0].length;
-        int res[][]=new int[rows][cols];
+
         Queue<int[]> queue=new LinkedList<>();
+
+
+        int res[][]=new int[rows][cols];
 
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
                 if(mat[i][j]==0){
-                    res[i][j]=0;
                     queue.add(new int[]{i,j});
+                    res[i][j]=0;
                 }
                 else{
                     res[i][j]=-1;
                 }
             }
-        }
+        } 
 
 
         while(!queue.isEmpty()){
@@ -28,35 +29,29 @@ class Solution {
                 int i=poll[0];
                 int j=poll[1];
 
-
                 if(i+1<rows && res[i+1][j]==-1){
-                    queue.add(new int[]{i+1,j});
                     res[i+1][j]=1+res[i][j];
+                    queue.add(new int[]{i+1,j});
                 }
-                
+
                 if(i-1>=0 && res[i-1][j]==-1){
-                    queue.add(new int[]{i-1,j});
                     res[i-1][j]=1+res[i][j];
+                    queue.add(new int[]{i-1,j});
                 }
 
                 if(j+1<cols && res[i][j+1]==-1){
-                    queue.add(new int[]{i,j+1});
                     res[i][j+1]=1+res[i][j];
+                    queue.add(new int[]{i,j+1});
                 }
 
                 if(j-1>=0 && res[i][j-1]==-1){
-                    queue.add(new int[]{i,j-1});
                     res[i][j-1]=1+res[i][j];
+                    queue.add(new int[]{i,j-1});
                 }
-
-            
             }
         }
 
 
         return res;
-
-
-        
     }
 }
