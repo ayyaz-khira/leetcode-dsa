07b66ab3a38ede30a1a43cea61bc41e694e26aef
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2026-06-15 10:11:02 +0530

    Time: 1 ms (99.71%) | Memory: 47.3 MB (37.87%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,14 +1,11 @@
 class Solution {
     public int jump(int[] nums) {
-
-        
-
+        int n=nums.length;
+        if(n==0 || n==1) return 0;
         int prevRange=0;
         int newRange=0;
-        int n=nums.length;
         int jumps=0;
 
-        if(n==1 || n==0) return 0;
 
         for(int i=0;i<n;i++){
             newRange=Math.max(newRange,nums[i]+i);
@@ -17,14 +14,13 @@ class Solution {
                 prevRange=newRange;
                 jumps++;
 
-                if(prevRange>=n-1){
+                if(newRange>=n-1){
                     return jumps;
                 }
             }
         }
 
 
-        return -1;
-
+        return jumps;
     }
 }
