9728fce132461b6077e5b9e1fc6e21a0e82a1271
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2026-06-15 11:31:30 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (80.06%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -2,24 +2,20 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int rows=obstacleGrid.length;
         int cols=obstacleGrid[0].length;
-
-        if(obstacleGrid[0][0]==1) return 0;
-
         int dp[][]=new int[rows][cols];
 
+
+        if(obstacleGrid[0][0]==1) return 0;
         dp[0][0]=1;
 
-
         for(int i=1;i<rows;i++){
-            if(obstacleGrid[i][0]==0){
-                dp[i][0]=dp[i-1][0];
-            }
+            if(obstacleGrid[i][0]==1) dp[i][0]=0;
+            else dp[i][0]=dp[i-1][0];
         }
 
         for(int j=1;j<cols;j++){
-            if(obstacleGrid[0][j]==0){
-                dp[0][j]=dp[0][j-1];
-            }
+            if(obstacleGrid[0][j]==1) dp[0][j]=0;
+            else dp[0][j]=dp[0][j-1];
         }
 
 
@@ -36,6 +32,5 @@ class Solution {
 
 
         return dp[rows-1][cols-1];
-
     }
 }
