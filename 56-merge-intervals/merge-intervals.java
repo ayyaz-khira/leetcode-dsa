fc6f0632a86f1d8d21ac6ce78e1a723928edb96b
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2026-06-16 10:07:18 +0530

    Time: 8 ms (90.83%) | Memory: 48.9 MB (83.31%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,24 +1,24 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+    
         List<int[]> list=new ArrayList<>();
-        Arrays.sort(intervals, (a,b)-> a[0]-b[0]);
 
-        for(int[] interval:intervals){
-            if(list.isEmpty() || list.get(list.size()-1)[1]<interval[0]){
+        for(int interval[]:intervals){
+            if(list.isEmpty() || interval[0]>list.get(list.size()-1)[1]){
                 list.add(interval);
             }
             else{
-                list.get(list.size()-1)[1]=Math.max(list.get(list.size()-1)[1],interval[1]);
+                list.get(list.size()-1)[1]=Math.max(interval[1],list.get(list.size()-1)[1]);
             }
-
         }
-
 
         int res[][]=new int[list.size()][];
         int i=0;
         for(int x[]:list){
             res[i++]=x;
         }
+
 
         return res;
     }
