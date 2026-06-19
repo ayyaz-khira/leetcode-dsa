fdfdae39a1dfa1c95281d848249dba224279140c
Ayyaz Khira <128171394+ayyaz-khira@users.noreply.github.com>
2026-06-19 12:06:51 +0530

    Time: 6 ms (85.23%) | Memory: 46.4 MB (82.65%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,68 +1,66 @@
-class Solution {
+class Pair{
+    int node;
+    int dist;
 
-    class Pair{
-        int node;
-        int dist;
-
-        Pair(int node,int dist){
-            this.node=node;
-            this.dist=dist;
-        }
+    Pair(int node,int dist){
+        this.node=node;
+        this.dist=dist;
     }
+}
 
 
+class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-
         ArrayList<ArrayList<Pair>> adj=new ArrayList<>();
+        int level=0;
+
+        int dist[]=new int[n];
+        Queue<Pair> queue=new LinkedList<>();
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[src]=0;
 
         for(int i=0;i<n;i++){
             adj.add(new ArrayList<>());
         }
 
-        for(int i=0;i<flights.length;i++){
-            int u=flights[i][0];
-            int v=flights[i][1];
-            int cost=flights[i][2];
+        for(int edges[]:flights){
+            int u=edges[0];
+            int v=edges[1];
+            int price=edges[2];
 
-            adj.get(u).add(new Pair(v,cost));
-        
+            adj.get(u).add(new Pair(v,price));
         }
 
-        int level=0;
-        Queue<Pair> queue=new LinkedList<>();
-
-        int res[]=new int[n];
-        Arrays.fill(res,Integer.MAX_VALUE);
-
         queue.add(new Pair(src,0));
+
 
         while(!queue.isEmpty() && level<=k){
 
             int size=queue.size();
             for(int i=0;i<size;i++){
-
             Pair pair=queue.poll();
-            int node=pair.node;
-            int dist=pair.dist;
+            int v=pair.node;
+            int cost=pair.dist;
 
-            for(Pair poll:adj.get(node)){
-                int pollNode=poll.node;
-                int pollDist=poll.dist;
 
-                if(pollDist+dist<res[pollNode]){
-                    res[pollNode]=pollDist+dist;
-                    queue.offer(new Pair(pollNode,res[pollNode]));
+
+            for(Pair p:adj.get(v)){
+                int adjNode=p.node;
+                int adjDist=p.dist;
+
+                if(cost+adjDist<dist[adjNode]){
+                    dist[adjNode]=cost+adjDist;
+                    queue.add(new Pair(adjNode,dist[adjNode]));
                 }
             }
-
             }
-            level+=1;
-
-
+            
+            level++;
         }
 
 
-        return res[dst]==Integer.MAX_VALUE ? -1 : res[dst];
+        return dist[dst]==Integer.MAX_VALUE ? -1 : dist[dst];
+
 
         
     }
